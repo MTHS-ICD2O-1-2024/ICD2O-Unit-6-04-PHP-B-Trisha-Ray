a1d42c20ff72c65a6438f69f176b17bd436399ed
default <trisha.ray@stu.ocsb.ca>
2025-05-26 17:24:22 +0000

    php not working

--- answer.php
+++ answer.php
@@ -4,16 +4,16 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Volume of a Sphere, with a Query String" />
+  <meta name="description" content="Volume of a Sphere, with PHP" />
   <meta name="keywords" content="mths, icd2o" />
-  <meta name="author" content="Mr. Coxall" />
+  <meta name="author" content="Trisha Ray" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link
     rel="stylesheet"
     href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <link
     rel="stylesheet"
-    href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+    href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
   <link rel="stylesheet" href="./css/style.css" />
   <link
     rel="apple-touch-icon"
@@ -29,30 +29,36 @@
     type="image/png"
     sizes="16x16"
     href="./favicon-16x16.png" />
-  <title>Volume of a Sphere, with a Query String</title>
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Volume of a Sphere, with PHP</title>
 </head>
 
 <body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Volume of a Sphere, with a Query String</span>
+        <span class="mdl-layout-title">Volume of a Sphere, with PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
         <img src="./images/volume-of-sphere.png" alt="Sphere Volume" />
       </div>
-      <div class="page-content">Sphere</div>
-      <br />
-      <p> solve for volume </p>
-      <br />
-      <div class="page-content">V=4/3πr³</div>
-      <form></form>
       <div class="page-content-php">
-          <p>Radius</p>
-        </form>
-      </div>
+        <div id="user-info">
+          <?php
+          $radius = $_GET["radius"];
+          // process
+          $volume = (4 / 3) * pi() * ($radius * $radius * $radius);
+          $roundNumber = round($volume, 3);
+          // output
+          echo "If radius is " . $radiusOfSphere . ", then the volume of the sphere is " . $roundNumber . " mm³.";
+          ?>
+          <div class="page-content-answer">
+            <a href="./index.php">Return</a>
+          </div>
+        </div>
     </main>
   </div>
 </body>
